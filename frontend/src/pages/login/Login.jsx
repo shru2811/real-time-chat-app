03a03013566b47c65d153a39f02a5ec422ee20d3
Shruti Srivastava <sshruti.2811@gmail.com>
2024-04-03 18:54:45 +0530

    Swift Talk

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   return <div className='flex flex-col items-center justify-center min-w-96 mx-auto '>
     <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">LOGIN
-            <span className="text-orange-600"> ChatApp</span>
+            <span className="text-orange-600"> SwiftTalk</span>
         </h1>
         <form onSubmit={handleSubmit}>
             <div>
