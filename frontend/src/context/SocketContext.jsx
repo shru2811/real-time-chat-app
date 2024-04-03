a5eb011d99fc77d9107239c61079bc9a3da5957d
Shruti Srivastava <sshruti.2811@gmail.com>
2024-04-03 18:47:46 +0530

    final checks

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://real-time-chat-app-p8hb.onrender.com", {
+			const socket = io("https://real-time-chat-web-app.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
