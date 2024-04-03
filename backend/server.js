6c1882fb6b8f902eb27a9d4ad84539a1782be590
Shruti Srivastava <sshruti.2811@gmail.com>
2024-04-03 16:55:41 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
 import {app,server} from './socket/socket.js';
 
